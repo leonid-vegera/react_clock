3ca0956349b9de123e848f36f4dfa2fcfed5d594
Leonid Vegera <leonid.vegera@gmail.com>
2021-05-24 00:06:48 +0300

    Updates

---
+++ precache-manifest.cd9f94ca403d0666ab72b9c56a4e45ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d737a99acdea3561696e3bdbf66277c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a1ab0a8cfc45834c300",
+    "url": "./static/css/main.989837b5.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1ab0a8cfc45834c300",
+    "url": "./static/js/main.8fc579f4.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
